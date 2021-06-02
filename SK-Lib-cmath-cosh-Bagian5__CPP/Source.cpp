d7a29c8ff866a6d5da08f8f025a6a0a310cc58d7
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-02 21:14:05 +0700

    Add project files.

---
+++ SK-Lib-cmath-cosh-Bagian5__CPP/Source.cpp
@@ -0,0 +1,22 @@
+// CPP program to demonstrate the cosh()
+// function
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by GeeksForGeeks
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main(){
+	double x = 3000.0, result;
+
+	result = cosh(x);
+	cout << "cosh(3000.0) = " << result << endl;
+
+	_getch();
+	return 0;
+}
